1e3b1eb5e89efbaec7301007b31146a6a1c2a6b9
MariaJulianaAguero <110269481+MariaJulianaAguero@users.noreply.github.com>
2023-11-06 20:52:47 -0300

    ..

--- script.js
+++ script.js
@@ -29,65 +29,6 @@ prevButton.addEventListener('click', prevImage);
 
 updateImage();
 
-/*Formulario*/
-function validarFormulario() {
-    
-    var nombre = document.getElementById("nombre").value;
-    var email = document.getElementById("email").value;
-    var edad = document.getElementById("edad").value;
-    var mensaje = document.getElementById("mensaje").value;
-    let errores = [];
- let campo_error = null;
- 
- let frm = document.getElementById("frm");
-    
-    var mensajeError = "";
 
-    
-    if (nombre.length === 0 || nombre.length > 30) {
-        mensajeError += "Nombre inválido. Debe tener entre 1 y 30 caracteres.\n";
-        document.getElementById("error-nombre").textContent = "Nombre inválido";
-    } else {
-        document.getElementById("error-nombre").textContent = "";
-    }
-
-    
-    if (email.length > 0 && !email.includes("@")) {
-        mensajeError += "Email inválido. Debe contener un @.\n";
-        document.getElementById("error-email").textContent = "Email inválido";
-    } else {
-        document.getElementById("error-email").textContent = "";
-    }
-
-    
-    if (edad < 18 || edad >= 100) {
-        mensajeError += "Edad inválida. Debe ser mayor o igual a 18 \n";
-        document.getElementById("error-edad").textContent = "Edad inválida";
-    } else {
-        document.getElementById("error-edad").textContent = "";
-    }
-
-    
-    if (mensaje.length === 0 || mensaje.length > 200) {
-        mensajeError += "Mensaje inválido. Debe tener entre 1 y 200 caracteres.\n";
-        document.getElementById("error-mensaje").textContent = "Mensaje inválido";
-    } else {
-        document.getElementById("error-mensaje").textContent = "";
-    }
-
-    
-    if (mensajeError !== "") {
-        alert("Corrija los siguientes errores:\n" + mensajeError);
-        return false;
-    }
-
-    
-    var datosIngresados = "Nombre: " + nombre + "<br>Email: " + email + "<br>Edad: " + edad + "<br>Mensaje: " + mensaje;
-    document.getElementById("datos-ingresados").innerHTML = datosIngresados;
-    document.getElementById("formulario").style.display = "none";
-    document.getElementById("mensaje-enviado").style.display = "block";
-
-    return false; 
-}
 
 
---
+++ validacion.js
@@ -0,0 +1,32 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    function validarFormulario() {
+        let valorName = document.forms["myform"]["nombre"].value;
+        let valorEmail = document.forms["myform"]["email"].value;
+        let valorEdad = document.forms["myform"]["edad"].value;
+        let valorMensaje = document.forms["myform"]["mensaje"].value;
+        let result = document.getElementById("resultado");
+
+        if (valorName === "" || valorEmail === "" || valorEdad === "" || valorMensaje === "") {
+            result.innerHTML = "<p>Todos los campos son obligatorios.</p>";
+            return false;
+        }
+
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (!valorEmail.match(emailPattern)) {
+            result.innerHTML = "<p>El email no es válido, asegúrese de escribirlo correctamente.</p>";
+            return false;
+        }
+
+       
+
+    if (valorEdad < 18 || valorEdad  >= 100) {
+        result.innerHTML = "<p>La edad no es válida, debe tener igual o mayor a 18.</p>";
+            return false;
+    }
+            result.innerHTML = "<p>¡Formulario Enviado Correctamente!!!</p>"
+            return false;        
+        }
+    document.forms["myform"].onsubmit = validarFormulario;
+    
+});
